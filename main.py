1b2b79bd3dffbbbf40e3d58575654a48e53391c1
Agamdeep Singh <agamdeep579@gmail.com>
2022-01-10 09:34:38 +0530

    Updated the code

--- main.py
+++ main.py
@@ -8,15 +8,35 @@
 
 import random
 
-# The Python file for the ASCII arts and the Hangman Logo
+# The Python files for the ASCII arts, the Hangman Logo and the words
 import assets.hangman_arts as hangman_arts
 from assets.hangman_words import word_list as words
 
 # Welcome message for the user
 def Welcome():
     print(hangman_arts.logo)
-    print("I have a word, can you guess it?")
+    Rules()
     
+def Rules():
+    # Give the player a choice to get to know How To Play
+    choice = input("\nDo you want to know the How To Play? Type (y/n): ").lower()
+    
+    # The list of How To Play
+    how_to_play = '''
+    - Hangman is game where you've a Word and you have to guess it letter by letter.
+    - You have 6 Lives to guess the word.
+    - When you make a wrong guess, you lose 1 life.
+    - If you lose all lives, you Lose.
+    - If you guess the word before losing all 6 lives, you Win.
+    '''
+
+    # Conditions to skip/toggle How To Play
+    if choice == 'y':
+        how_to_play
+    
+    else:
+        print("\nI have a word, can you guess it?")
+        Main()
 
 # The main code of the game
 def Main():
